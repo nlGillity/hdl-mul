a95d68ed4777f8455c6d856c05fce1026786135e
Danil <121670088+nlGillity@users.noreply.github.com>
2026-05-01 11:21:19 +0300

    Added the multiplier module with demo tb

---
+++ mul/mul.sv
@@ -0,0 +1,197 @@
+`timescale 10ns/100ps;
+
+module mul #(
+    localparam int DATA_WIDTH = 8
+)(
+    input  logic                      clk,
+    input  logic                      rstn,
+
+    input  logic                      up_vld,
+    output logic                      up_ready,
+    input  logic [  DATA_WIDTH - 1:0] a,
+    input  logic [  DATA_WIDTH - 1:0] b,
+
+    output logic                      down_vld,
+    input  logic                      down_ready,
+    output logic [2*DATA_WIDTH - 1:0] res
+);
+
+    //=======================================================================
+    // Flow control
+    //=======================================================================
+
+    localparam int LAST = DATA_WIDTH / 2 - 1; 
+
+    wire  [DATA_WIDTH / 2 - 1:0] flow_en;
+    logic [DATA_WIDTH / 2 - 1:0] valid_ff;
+
+    always_ff @(posedge clk or negedge rstn) begin
+        if (~rstn) valid_ff <= '0;
+        else begin
+            for (int i = 1; i <= LAST; ++i) begin
+                if (flow_en[i]) 
+                    valid_ff[i] <= valid_ff[i - 1];
+            end
+            if (flow_en[0]) 
+                valid_ff[0] <= up_vld; 
+        end
+    end
+
+
+    generate
+        assign flow_en[LAST] = down_ready | ~valid_ff[LAST];
+        for (genvar i = 0; i < LAST; ++i)
+            assign flow_en[i] = flow_en[i + 1] | ~valid_ff[i];
+    endgenerate
+
+    //=======================================================================
+    // Valid-ready
+    //=======================================================================
+
+    assign up_ready = flow_en[0];
+    assign down_vld = valid_ff[LAST];
+
+    //=======================================================================
+    // Booth recoding stage
+    //=======================================================================
+
+    wire [DATA_WIDTH + 1:0] pp  [0:DATA_WIDTH / 2 - 1];
+    wire [             2:0] sel [0:DATA_WIDTH / 2 - 1];
+
+    generate
+        for (genvar i = 0; i < DATA_WIDTH / 2; i++) begin
+            if (i == 0) assign sel[i] = { b[1:0], 1'b0 };
+            else        assign sel[i] = b[(2*i + 1) : (2*i - 1)];
+        end
+    endgenerate
+
+    generate
+        for (genvar i = 0; i < DATA_WIDTH / 2; i += 1) begin
+            radix4 #(
+                .DATA_WIDTH (DATA_WIDTH)
+            ) booth_recoding (
+                .num ( a       ),
+                .sel ( sel [i] ),
+                .pp  ( pp  [i] )
+            );
+        end
+    endgenerate
+
+    // Buffering to next stage
+    //------------------------------------------------------------------------
+
+    logic [DATA_WIDTH + 1:0] pp_ff [0:DATA_WIDTH / 2 - 1];
+
+    always_ff @(posedge clk) begin
+        if (flow_en[0] & valid_ff[0])
+            for (int i = 0; i < DATA_WIDTH / 2; ++i)
+                pp_ff[i] <= pp[i];
+    end
+
+    //=======================================================================
+    // Wallace tree stage
+    //=======================================================================
+
+    // Stage 1
+    //------------------------------------------------------------------------
+
+    wire [15:0] pp0_stage1;
+    wire [12:0] pp1_stage1;
+
+    assign pp0_stage1[1:0] = pp_ff[0][1:0];
+
+    csa #(
+        .DATA_WIDTH (2)
+    ) ha1_stage1 (
+        .a    ( pp_ff   [0][3:2] ),
+        .b    ( pp_ff   [1][1:0] ),
+        .cin  ( 2'b0             ),
+
+        .sum  ( pp0_stage1 [3:2] ),
+        .cout ( pp1_stage1 [1:0] ) 
+    );
+
+    csa #(
+        .DATA_WIDTH (12)
+    ) fa1_stage1 (
+        .a    ( { {6{pp_ff[0][9]}}, pp_ff[0][9:4] } ),
+        .b    ( { {4{pp_ff[1][9]}}, pp_ff[1][9:2] } ),
+        .cin  ( { {2{pp_ff[2][9]}}, pp_ff[2]      } ),
+
+        .sum  ( pp0_stage1 [15:4] ),
+        .cout ( pp1_stage1 [12:2] ) 
+    );
+
+    // Buffering to next stage
+    //------------------------------------------------------------------------
+
+    logic [15:0] pp0_stage1_ff;
+    logic [12:0] pp1_stage1_ff;
+    logic [ 9:0] pp3_ff;
+
+    always_ff @(posedge clk) begin
+        if (flow_en[1] & valid_ff[1]) begin
+            pp0_stage1_ff <= pp0_stage1;
+            pp1_stage1_ff <= pp1_stage1;
+            pp3_ff        <= pp_ff[3];
+        end
+    end
+
+    // Stage 2
+    //------------------------------------------------------------------------
+
+    wire [15:0] pp0_stage2;
+    wire [11:0] pp1_stage2;
+
+    assign pp0_stage2[2:0] = pp0_stage1_ff[2:0];
+
+    csa #(
+        .DATA_WIDTH (3)
+    ) ha1_stage2 (
+        .a    ( pp0_stage1_ff [5:3] ),
+        .b    ( pp1_stage1_ff [2:0] ),
+        .cin  ( 3'b0                ),
+
+        .sum  ( pp0_stage2    [5:3] ),
+        .cout ( pp1_stage2    [2:0] ) 
+    );
+
+    csa #(
+        .DATA_WIDTH (10)
+    ) fa1_stage2 (
+        .a    ( pp0_stage1_ff [15:6] ),
+        .b    ( pp1_stage1_ff [12:3] ),
+        .cin  ( pp3_ff               ),
+
+        .sum  ( pp0_stage2    [15:6] ),
+        .cout ( pp1_stage2    [11:3] ) 
+    );
+
+    // Buffering to next stage
+    //------------------------------------------------------------------------
+
+    logic [15:0] pp0_stage2_ff;
+    logic [11:0] pp1_stage2_ff;
+
+    always_ff @(posedge clk) begin
+        if (flow_en[2] & valid_ff[2]) begin
+            pp0_stage2_ff <= pp0_stage2;
+            pp1_stage2_ff <= pp1_stage2;
+        end
+    end
+
+    //=======================================================================
+    // Final sum stage
+    //=======================================================================
+
+    wire [2*DATA_WIDTH - 1:0] sum;
+    assign sum = { pp0_stage2_ff[15:4] + pp1_stage2_ff, pp0_stage2_ff[3:0] };
+
+    // Buffering to output
+    //------------------------------------------------------------------------
+
+    always_ff @(posedge clk) begin
+        if (flow_en[LAST] & valid_ff[LAST]) res <= sum;
+    end
+
+endmodule
---
+++ mul/mul_tb.sv
@@ -0,0 +1,146 @@
+`timescale 10ns/100ps;
+
+module mul_tb;
+
+    localparam int DATA_WIDTH     = 8;
+    localparam int CLOCK_PERIOD   = 10;
+    localparam int RESET_DURATION = 20;
+    localparam int TEST_NUM       = 500;
+
+    logic clk;
+    logic rstn;
+    int   clk_cnt;
+
+    logic error_stob;
+    int   error_cnt;
+    
+    //====================================================================
+    // Wiring with DUT and check variables
+    //====================================================================
+
+    logic                      up_vld,     next_up_vld;
+    logic                      up_ready;
+    logic [  DATA_WIDTH - 1:0] a,          next_a;
+    logic [  DATA_WIDTH - 1:0] b,          next_b;
+
+    logic                      down_vld;
+    logic                      down_ready, next_down_ready;
+    logic [2*DATA_WIDTH - 1:0] res;
+
+    int signed expected;
+
+    //====================================================================
+    // Device Under Test
+    //====================================================================
+
+    mul DUT (
+        .clk        ( clk        ),
+        .rstn       ( rstn       ),
+
+        .up_vld     ( up_vld     ),
+        .up_ready   ( up_ready   ),
+        .a          ( a          ),
+        .b          ( b          ),
+
+        .down_vld   ( down_vld   ),
+        .down_ready ( down_ready ),
+        .res        ( res        )
+    );
+
+    //====================================================================
+    // Genetrial proccesses/tasks
+    //====================================================================
+
+    initial begin
+        $dumpfile("sim.vcd");
+        $dumpvars(0, mul_tb);
+    end
+
+    initial begin
+        clk <= 1'b0;
+        forever #(CLOCK_PERIOD / 2) clk <= ~clk;
+    end
+
+    initial begin
+        forever begin
+            @(posedge clk);
+            clk_cnt += 1;
+        end
+    end
+
+    task reset();
+        rstn       <= 1'b0;
+        a          <= '0;
+        b          <= '0;
+        up_vld     <= 1'b0;
+        down_ready <= 1'b0;
+        error_stob <= 1'b0;
+        repeat (RESET_DURATION) @(posedge clk);
+        rstn      <= 1'b1;
+    endtask
+
+    //====================================================================
+    // Funbctions
+    //====================================================================
+
+    // DUT stimulation functions
+    //--------------------------------------------------------------------
+
+    function void prepare();
+        next_a          = $urandom_range(10, 255);
+        next_b          = $urandom_range(10, 255);
+        next_up_vld     = $urandom_range(1, 0);
+        next_down_ready = $urandom_range(1, 0);
+        expected        = signed'(next_a) * signed'(next_b);
+    endfunction
+
+    task drive();
+        a          <= next_a;
+        b          <= next_b;
+        up_vld     <= next_up_vld;
+        down_ready <= next_down_ready;
+    endtask
+
+    task delay( input int min, input int max );
+        repeat ($urandom_range(min, max)) @(posedge clk);
+    endtask
+
+    // Check functions
+    //--------------------------------------------------------------------
+
+    task stobe();
+        error_stob <= 1'b1;
+        @(posedge clk);
+        error_stob <= 1'b0;
+    endtask
+
+    task check();
+        if (signed'(res) !== expected) begin
+            $display(
+                "Error at clk #%0d [a = %0d, b = %0d] real = %0d, expected = %0d",
+                clk_cnt, a, b, signed'(res), expected
+            );
+            error_cnt += 1;
+            stobe();
+        end
+    endtask
+
+    //====================================================================
+    // Testbench running
+    //====================================================================
+
+    initial begin
+        reset();
+        for (int i = 0; i < TEST_NUM; ++i) begin
+            prepare();
+            drive  ();
+            // @(posedge clk);
+            // check  ();
+            delay  (1, 10);
+        end
+        if (error_cnt) $display("FAILED");
+        else           $display("PASSED");
+        $finish();
+    end
+
+endmodule
