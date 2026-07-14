a8a3e4f769b372601d50ecb311462f7fe70b4a42
Danil <dinozavar137@gmail.com>
2026-07-14 23:40:33 +0300

    feat(tb): add env

--- tb/uvm/env/valid_ready_env.sv
+++ tb/uvm/env/valid_ready_env.sv
@@ -25,7 +25,11 @@ class valid_ready_env #(
     // Properties
     //--------------------------------------------------------------------
     
+    // Configurator
+    env_cfg_t cfg;
+    // Master agent
     agent_t master;
+    // Slave agent
     agent_t slave;
     
     //--------------------------------------------------------------------
@@ -34,6 +38,32 @@ class valid_ready_env #(
     
     extern function new(string name, uvm_component parent);
     extern virtual function void build_phase(uvm_phase phase);
-    extern virtual function void connect_phase(uvm_phase phase);
     
 endclass : valid_ready_env
+
+
+function valid_ready_env::new(string name, uvm_component parent);
+    super.new(name, parent);
+endfunction : new
+
+
+function valid_ready_env::build_phase(uvm_phase phase);
+    super.build_phase(phase);
+    
+    if (!uvm_config_db#(env_cfg_t)::get(this, "", "cfg", cfg))
+        `uvm_fatal({get_full_name(), ".db_get_fail"}, "Cannot get configs.")
+    
+    set_inst_override_by_type(
+            "master.driver",
+            valid_ready_driver#(PARAMS)::get_type(),
+            valid_ready_master_driver#(PARAMS)::get_type()
+    );
+    master = agent_t::type_id::create("master");
+    
+    set_inst_override_by_type(
+            "slave.driver",
+            valid_ready_driver#(PARAMS)::get_type(),
+            valid_ready_slave_driver#(PARAMS)::get_type()
+    );
+    slave = agent_t::type_id::create("slave");
+endfunction : build_phase
